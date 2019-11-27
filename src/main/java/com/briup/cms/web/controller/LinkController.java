2586695b7211098508fa5464b509b098b26044ff
yaoby <1724868042@qq.com>
2019-11-27 23:36:44 +0800

    新增了根据id删除链接

--- src/main/java/com/briup/cms/web/controller/LinkController.java
+++ src/main/java/com/briup/cms/web/controller/LinkController.java
@@ -5,8 +5,10 @@ import com.briup.cms.service.ILinkService;
 import com.briup.cms.util.Message;
 import com.briup.cms.util.MessageUtil;
 
+import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +23,13 @@ public class LinkController {
     @ApiOperation("添加链接")
     public Message addlink(Link link){
         linkService.saveOrUpdate(link);
+        return MessageUtil.success();
+    }
+    @GetMapping("/deleteLink")
+    @ApiOperation("根据id删除链接")
+    @ApiImplicitParam(name = "id" ,value = "链接id",paramType = "query",dataType = "int",required = true)
+    public Message deleteLink(int id){
+        linkService.deleteLink(id);
         return MessageUtil.success();
     }
 }
