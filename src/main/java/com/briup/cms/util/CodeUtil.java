735b70ad97ba207d8784621c6e91da440e16253c
yaoby <1724868042@qq.com>
2019-11-27 22:47:36 +0800

    初始化项目及链接管理功能

---
+++ src/main/java/com/briup/cms/util/CodeUtil.java
@@ -0,0 +1,8 @@
+package com.briup.cms.util;
+
+public interface CodeUtil {
+    /**
+     * 系统状态异常码
+     */
+    int BAD_CODE = 500;
+}
---
+++ src/main/java/com/briup/cms/web/controller/LinkController.java
@@ -0,0 +1,28 @@
+package com.briup.cms.web.controller;
+
+import com.briup.cms.bean.Link;
+import com.briup.cms.service.ILinkService;
+import com.briup.cms.util.Message;
+import com.briup.cms.util.MessageUtil;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/link")
+
+public class LinkController {
+    @Autowired
+    private ILinkService linkService;
+    @PostMapping("/add")
+    @ApiOperation("添加链接")
+    public Message addlink(Link link){
+        linkService.saveOrUpdate(link);
+        return MessageUtil.success();
+    }
+}
+
+
